e38566395f6d6352d7fc531a4073219bc957e61b
b07902055 <b07902055@ntu.edu.tw>
2020-05-09 23:13:29 +0800

    set DEBUG to False

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%-_nu(l4-q6^@e(#&f2+u_ul_4&9))yy2n+wt0^++zjcn&h=zh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
