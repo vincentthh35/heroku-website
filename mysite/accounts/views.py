8d02b75ce9d5e34027529e61fd7241ed324e4664
b07902055 <b07902055@ntu.edu.tw>
2020-05-09 23:51:42 +0800

    debug on

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -54,7 +54,7 @@ def signup(request):
             user.save()
             current_site = get_current_site(request)
             mail_subject = 'Activate your account...'
-            message = render_to_string('account_activation_email.html', {
+            message = render_to_string('registration/account_activation_email.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
