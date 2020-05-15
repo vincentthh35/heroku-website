ae08bf69de2d041f55e9f2f19c72efead9ed1b9a
b07902055 <b07902055@ntu.edu.tw>
2020-05-15 15:23:28 +0800

    add HistoricalPrices in models

--- mysite/stock/models.py
+++ mysite/stock/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class HistoricalPrices(models.Model):
+    date = models.DateField()
+    stock_id = models.CharField(max_length=10)
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    volume = models.DecimalField(max_digits=20, decimal_places=0)
