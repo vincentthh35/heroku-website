a8a95d4fcf53ddaf99b9bbe04be017f7a992e9fe
b07902055 <b07902055@ntu.edu.tw>
2020-10-05 14:28:42 +0800

    add clock_script.py for testing

---
+++ mysite/stock/management/commands/clock_script.py
@@ -0,0 +1,105 @@
+import yfinance as yf
+from datetime import datetime
+import numpy as np
+from tqdm import tqdm
+
+# import models
+from stock.models import StockRecord, StockInfo
+
+'''
+** find all objects
+StockRecord.objects.all()
+    return QuerySet
+
+** find one type of objects
+StockRecord.objects.filter(variable='value')
+    return QuerySet
+
+** find if object exists
+StockRecord.objects.filter(variable='value').exists()
+    return True or False
+'''
+
+RANKING_SIZE = 25
+
+
+# every 2pm on weekdays
+# Ranking Part
+def getBuiltInRankings():
+    stock_list = StockInfo.objects.all()
+    list_size = len(stock_list)
+
+    # check validity of data
+    stock = stock_list[0]
+    ticker = yf.Ticker(f'{stock.ticker}.TW')
+    his = ticker.history(period='1d', actions=False)
+    # valid
+    if datetime.today().date() == his.index[0]:
+        rise = np.zeros(list_size)
+        fall = np.zeros(list_size)
+        volume = np.zeros(list_size)
+
+        for i in tqdm(range(list_size)):
+            stock = stock_list[i]
+            ticker = yf.Ticker(f'{stock.ticker}.TW')
+            his = ticker.history(period='1d', actions=False)
+            if his.empty:
+                continue
+            # rise, fall, volume
+            rise[i] = (his['Close'][0] - his['Open'][0]) / his['Close'][0]
+            fall[i] = -rise[i]
+            volume[i] = his['Volume'][0]
+
+        # sort
+        top_rise = rise.argsort()[-RANKING_SIZE: ][ : : -1]
+        top_fall = fall.argsort()[:RANKING_SIZE]
+        top_volume = volume.argsort()[-RANKING_SIZE: ][ : : -1]
+
+        # delete old records
+        # consider deleting all of them? yes?
+        if StockRecord.objects.all().exists:
+            StockRecord.objects.all().delete()
+
+        # store data into database
+        # Model.objects.bulk_create([ Model(**{variable: value, ...}) for m in list ])
+        StockRecord.objects.bulk_create([
+            StockRecord(**{
+                'record_type': 'rise',
+                'stock_info': stock_list[ int(top_rise[i]) ],
+                'ranking_number': i + 1,
+                'last_modified': his.index[0]
+            }) for i in range(RANKING_SIZE)
+        ])
+        StockRecord.objects.bulk_create([
+            StockRecord(**{
+                'record_type': 'fall',
+                'stock_info': stock_list[ int(top_fall[i]) ],
+                'ranking_number': i + 1,
+                'last_modified': his.index[0]
+            }) for i in range(RANKING_SIZE)
+        ])
+        StockRecord.objects.bulk_create([
+            StockRecord(**{
+                'record_type': 'volume',
+                'stock_info': stock_list[ int(top_volume[i]) ],
+                'ranking_number': i + 1,
+                'last_modified': his.index[0]
+            }) for i in range(RANKING_SIZE)
+        ])
+
+    # for stock in stock_list:
+    #
+    # ticker = yf.Ticker(f'{ticker}.TW')
+    # his = ticker.history(period="__", interval="__", action=False)
+    # value = abstract.Function(indicator_name, timeperiod=time_period)
+
+# import
+from django.core.management.base import BaseCommand
+
+# Command class (the class name is fixed)
+class Command(BaseCommand):
+    help = 'for heroku run clock.py'
+
+    # command method (the function name is fixed)
+    def handle(self, *args, **options):
+        getBuiltInRankings()
